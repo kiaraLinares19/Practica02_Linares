a53779d591889ba21cb926c3275c32704126a75f
kiaraLinares19 <kiaralinaresestivariz@gmail.com>
2025-09-26 21:05:18 -0500

    Rama1

--- Data/Migrations/20250927020121_ModelosIniciales.Designer.cs
+++ Data/Migrations/20250927020121_ModelosIniciales.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Practica2.Data;
 
@@ -10,9 +11,11 @@ using Practica2.Data;
 namespace Practica2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250927020121_ModelosIniciales")]
+    partial class ModelosIniciales
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
