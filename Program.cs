17bed0c13509ce376a7533dde4a18081bc16f477
kiaraLinares19 <kiaralinaresestivariz@gmail.com>
2025-09-27 03:55:29 -0500

    cambiosbasededatos

--- Program.cs
+++ Program.cs
@@ -1,35 +1,24 @@
+
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Practica2.Data;
 using Microsoft.Extensions.Caching.StackExchangeRedis; 
-using Npgsql.EntityFrameworkCore.PostgreSQL;
-using Microsoft.AspNetCore.Hosting; 
-using Microsoft.Extensions.Logging;
-using Practica2.Extensions; 
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-    ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") 
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    
-    options.UseNpgsql(connectionString);
-});
-
+    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
-
 
 
 var redisConnectionString = builder.Configuration.GetConnectionString("RedisConnection") 
@@ -59,59 +48,32 @@ else
     builder.Services.AddSession();
 }
 
-
-
 var app = builder.Build();
-
 
 using (var scope = app.Services.CreateScope())
 {
     var serviceProvider = scope.ServiceProvider;
     try
     {
-        
         await Practica2.Data.IdentitySeeder.SeedRolesAsync(serviceProvider);
-        
-        
-        var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
-        await dbContext.Database.MigrateAsync();
     }
     catch (Exception ex)
     {
         var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the identity roles or migrating the database.");
+        logger.LogError(ex, "An error occurred while seeding the identity roles and users.");
     }
 }
 
 
-
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseSession(); 
+app.UseSession();
+
+
 app.UseAuthorization();
-
-
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-
 
 app.MapControllerRoute(
     name: "default",
